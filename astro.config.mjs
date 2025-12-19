a454fb35bbb78296afa877ce2435e47ac465f76e
zane <czl970721@gmail.com>
2025-12-18 19:47:34 -0500

    fix canonical

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-  site: "https://kiyo-n-zane.com",
+  site: "https://www.kiyo-n-zane.com",
   trailingSlash: "ignore",
   compressHTML: true,
   env: {
